3fd505669123ff3227acc6a2cf2f7f49773a6d0c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-11-30 18:52:47 +0300

    Working merge for basic case.

--- src/tailwind-classes.lisp
+++ src/tailwind-classes.lisp
@@ -9,10 +9,33 @@
 (in-package #:tailwind-merge/tailwind-classes)
 
 
+(-> integer-value-p ((or null string))
+    (values boolean &optional))
+
+(defun integer-value-p (value)
+  (when (and value
+             (ignore-errors (parse-integer value)))
+    (values t)))
+
+
 (defparameter *classes*
-  '((:px)
+  '(;; Padding classes are not conflicting to each other.
+    ;; We can have p-8 px-2 and it is ok.
+    ;; But when user specified p-8 px-2 p-4 px-1, then
+    ;; after the merge it should be p-4 px-1
+    (:px)
     (:py)
+    (:pl)
+    (:pt)
+    (:pr)
+    (:pb)
     (:p)
+    (:border-width (:border null)) ;; This is for processing "border" class
+    (:border-width (:border integer-value-p)) ;; When we have border-2, border-3, etc, this is boder-width too
+    (:border-color (:border
+                    ;; Tailwind color predicate function should be created instead
+                    :red
+                    :green))
     (:text-color (:text :red :green))
     (:text-size (:text :xl :2xl))
     (:text-align :text-left :text-center)))
@@ -46,9 +69,9 @@
         do (loop for rule in rules
                  do (etypecase rule
                       (keyword
-                         (setf (gethash (string-downcase rule)
-                                        result)
-                               class-name))
+                         (push class-name
+                               (gethash (string-downcase rule)
+                                        result)))
                       (cons
                          (let ((prefix (string-downcase (first rule)))
                                (validators (make-validators-from-rule (rest rule))))
@@ -60,34 +83,37 @@
 
 
 
-(defun parse-class (string)
+(defun parse-class (string &aux (map (build-classes-map)))
   "Parses CSS class and returns an object."
   ;; Finds prefixes from string end to the start while searching a parser for the prefix.
 
-  (loop with end-pos = (length string)
-        with map = (build-classes-map)
-        for separator-pos = (position #\- string
-                                      :from-end t
-                                      :end end-pos
-                                      :test #'char=)
-        when separator-pos
-          do (let* ((prefix (subseq string 0 separator-pos))
-                    (value (subseq string (1+ separator-pos)))
-                    (class-name-or-validators
-                      (gethash prefix map)))
-               (when class-name-or-validators
-                 (etypecase class-name-or-validators
-                   (keyword (return-from parse-class
-                              class-name-or-validators))
-                   (list (loop for (class-name . validators) in class-name-or-validators
-                               do (loop for validator in validators
-                                        always (funcall validator value)
-                                        finally (return-from parse-class
-                                                  class-name))))))
-               (setf end-pos
-                     (1- separator-pos)))
-        while (and separator-pos
-                   (< 0 separator-pos))))
+  (flet ((search-for-class (prefix &optional value)
+           (let ((class-name-or-validators
+                   (gethash prefix map)))
+             (when class-name-or-validators
+               (loop for (class-name . validators) in class-name-or-validators
+                     do (loop for validator in validators
+                              always (funcall validator value)
+                              finally (return-from parse-class
+                                        class-name)))))))
+    (loop with end-pos = (length string)
+          with separator-was-found = nil
+          for separator-pos = (position #\- string
+                                        :from-end t
+                                        :end end-pos
+                                        :test #'char=)
+          when separator-pos
+            do (setf separator-was-found
+                     t)
+               (let* ((prefix (subseq string 0 separator-pos))
+                      (value (subseq string (1+ separator-pos))))
+                 (search-for-class prefix value)
+                 (setf end-pos
+                       (1- separator-pos)))
+          while (and separator-pos
+                     (< 0 separator-pos))
+          finally (unless separator-was-found
+                    (return (search-for-class string))))))
 
 
 
